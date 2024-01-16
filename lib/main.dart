b273c070706d500617ecae724cdcc0e4486451c7
GEANO Lorenzo <[lorenzo.geano@etu.univ-amu.fr]>
2024-01-16 18:38:55 +0100

    debut de la creation de login il manque la mise en style et le logo + connexion bdd

--- lib/main.dart
+++ lib/main.dart
@@ -18,88 +18,230 @@ class BrainMusicApp extends StatelessWidget {
   }
 }
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       body: Padding(
         padding: EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            // App name
-            Text(
-              'BrainMusic',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 80.0), // Space between text and input fields
-
-            // Email input field
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: 'Email',
-                hintStyle: TextStyle(color: Colors.white54),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blue),
+        child: Container(
+          width: 328,
+          height: 315,
+          margin: EdgeInsets.only(top: 299, left: 16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Text(
+                'BrainMusic',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-              style: TextStyle(color: Colors.white),
-              keyboardType: TextInputType.emailAddress,
-            ),
-            SizedBox(height: 20.0), // Space between input fields
-
-            // Password input field
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: 'Mot de passe',
-                hintStyle: TextStyle(color: Colors.white54),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
+              SizedBox(height: 30),
+              TextFormField(
+                controller: emailController,
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  hintStyle: TextStyle(color: Colors.white54),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blue),
+                  ),
                 ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blue),
+                style: TextStyle(color: Colors.white),
+                keyboardType: TextInputType.emailAddress,
+              ),
+              SizedBox(height: 20.0),
+              TextFormField(
+                controller: passwordController,
+                decoration: InputDecoration(
+                  hintText: 'Mot de passe',
+                  hintStyle: TextStyle(color: Colors.white54),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blue),
+                  ),
+                ),
+                obscureText: true,
+                style: TextStyle(color: Colors.white),
+              ),
+              SizedBox(height: 40.0),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.purple,
+                  onPrimary: Colors.white,
+                ),
+                onPressed: () {
+                  String email = emailController.text;
+                  String password = passwordController.text;
+                  print('Email: $email');
+                  print('Mot de passe: $password');
+
+                  // TODO: Implémenter la logique de connexion
+
+                  // Naviguer vers la page du profil après la connexion
+                  //   Navigator.pushReplacement(
+                  //     context,
+                  //     MaterialPageRoute(builder: (context) => ProfileScreen()),
+                  //   );
+                  //
+                },
+                child: Text('Se connecter'),
+              ),
+              SizedBox(height: 20.0),
+              OutlinedButton(
+                style: OutlinedButton.styleFrom(
+                  side: BorderSide(color: Colors.white),
+                  foregroundColor: Colors.white,
+                ),
+                onPressed: () {
+                  // Naviguer vers l'écran d'inscription
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SignUpScreen()),
+                  );
+                },
+                child: Text("S'inscrire"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SignUpScreen extends StatefulWidget {
+  @override
+  _SignUpScreenState createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmPasswordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Container(
+          width: 328,
+          height: 375, // Ajuster la hauteur en fonction des éléments ajoutés
+          margin: EdgeInsets.only(top: 299, left: 16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Text(
+                'BrainMusic',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-              obscureText: true,
-              style: TextStyle(color: Colors.white),
-            ),
-            SizedBox(height: 40.0), // Space between input fields and button
-
-            //
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Colors.purple, // background color
-                onPrimary: Colors.white, // foreground color
+              SizedBox(height: 30),
+              TextFormField(
+                controller: emailController,
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  hintStyle: TextStyle(color: Colors.white54),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blue),
+                  ),
+                ),
+                style: TextStyle(color: Colors.white),
+                keyboardType: TextInputType.emailAddress,
               ),
-              onPressed: () {
-                // TODO: Implement login logic
-              },
-              child: Text('Se connecter'),
-            ),
-            SizedBox(height: 20.0), // Space between buttons
-
-// S'inscrire button
-            OutlinedButton(
-              style: OutlinedButton.styleFrom(
-                side: BorderSide(color: Colors.white), // border color
-                foregroundColor: Colors.white, // foreground color
+              SizedBox(height: 20.0),
+              TextFormField(
+                controller: passwordController,
+                decoration: InputDecoration(
+                  hintText: 'Mot de passe',
+                  hintStyle: TextStyle(color: Colors.white54),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blue),
+                  ),
+                ),
+                obscureText: true,
+                style: TextStyle(color: Colors.white),
               ),
-              onPressed: () {
-                // TODO: Implement registration logic
-              },
-              child: Text("S'inscrire"),
-            ),
-          ],
+              SizedBox(height: 20.0),
+              TextFormField(
+                controller: confirmPasswordController,
+                decoration: InputDecoration(
+                  hintText: 'Confirmer mot de passe',
+                  hintStyle: TextStyle(color: Colors.white54),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blue),
+                  ),
+                ),
+                obscureText: true,
+                style: TextStyle(color: Colors.white),
+              ),
+              SizedBox(height: 40.0),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.purple,
+                  onPrimary: Colors.white,
+                ),
+                onPressed: () {
+                  String email = emailController.text;
+                  String password = passwordController.text;
+                  String confirmPassword = confirmPasswordController.text;
+                  print('Email: $email');
+                  print('Mot de passe: $password');
+                  print('Confirmer mot de passe: $confirmPassword');
+                  // TODO: Implémenter la logique d'inscription
+                },
+                child: Text("S'inscrire"),
+              ),
+              SizedBox(height: 20.0),
+              GestureDetector(
+                onTap: () {
+                  // Naviguer vers l'écran de connexion
+                  Navigator.pop(context);
+                },
+                child: Text(
+                  'Déjà un compte ? Connectez-vous',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
