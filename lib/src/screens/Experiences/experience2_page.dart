8fa748815a9c36c3c869d6576558dacf0138f6b3
theb1ls <bilal.barar@etu.univ-amu.fr>
2024-01-18 11:22:52 +0100

    Création de la page experience

---
+++ lib/src/screens/Experiences/experience2_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Experience2 extends StatelessWidget {
+  const Experience2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Text("Hello"));
+  }
+}
