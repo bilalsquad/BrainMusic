47da808e3dabfeb29ab4f1501bed36ef59252ee5
GEANO Lorenzo <[lorenzo.geano@etu.univ-amu.fr]>
2024-01-19 17:58:58 +0100

    ajotu du fichier musique volumineux

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,14 +6,12 @@ import FlutterMacOS
 import Foundation
 
 import audioplayers_darwin
-import flutter_blue
 import path_provider_foundation
 import shared_preferences_foundation
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
-  FlutterBluePlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
