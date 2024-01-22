86a12b160c3f62bcbd622037b5efbb7b3e1ca6fa
GEANO Lorenzo <[lorenzo.geano@etu.univ-amu.fr]>
2024-01-22 18:31:20 +0100

    ajout des verif

---
+++ lib/services/valideurInput.dart
@@ -0,0 +1,6 @@
+bool isValidEmail(String email) {
+  String emailPattern = r'^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$';
+  RegExp regExp = RegExp(emailPattern);
+  return regExp.hasMatch(email);
+  //return true;
+}
